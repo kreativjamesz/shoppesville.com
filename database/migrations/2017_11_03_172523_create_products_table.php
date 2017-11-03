563b03443a2fa96eb9b333127fad2573030a323e
kreativjamesz <kreativjames@gmail.com>
2017-11-04 03:12:38 +0800

    Updated @ November 4, 2017

---
+++ database/migrations/2017_11_03_172523_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('prod_name');
+            $table->text('prod_desc');
+            $table->string('prod_img');
+            $table->string('prod_srp_price');
+            $table->string('prod_sale_price');
+            $table->string('prod_orig_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin', 'middleware' => ['role:administrator','role:superadministrator']], function() {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
+
+Route::get('/', 'Front@index');
+// Route::get('/products', 'Front@products');
+// Route::get('/products/details/{id}', 'Front@product_details');
+// Route::get('/products/categories/{name}', 'Front@product_categories');
+// Route::get('/products/brands/{name}/{category?}', 'Front@product_brands');
+// Route::get('/blog', 'Front@blog');
+// Route::get('/blog/{id}', 'Front@blog_post');
+// Route::get('/contact-us', 'Front@contact_us');
